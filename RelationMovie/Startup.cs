c9850a8c46181d74100e4640cdd0eab2bc520e21
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-02-17 22:40:37 +0700

    Add project files.

---
+++ RelationMovie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RelationMovie.Startup))]
+namespace RelationMovie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
