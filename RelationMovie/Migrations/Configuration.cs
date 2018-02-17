6d1a169b1aa2545d7a43d691a6e9622cdfb6cb0f
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-02-18 01:26:39 +0700

    dbContext

--- RelationMovie/Migrations/Configuration.cs
+++ RelationMovie/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace RelationMovie.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = true;
         }
 
