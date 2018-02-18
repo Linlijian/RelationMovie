84c5c57b32389ca0f58fe052bca55dcd26769f9d
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-02-19 03:06:40 +0700

    Test DB [Key] property

---
+++ RelationMovie/Entities/test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RelationMovie.Entities
+{
+    [Table("Test")]
+    public class Test
+    {
+        [Key]
+        public int AIdA { get; set; }
+
+        public int AIdA1 { get; set; }
+
+        public int AIdA2 { get; set; }
+
+    }
+}
--- RelationMovie/Models/IdentityModels.cs
+++ RelationMovie/Models/IdentityModels.cs
@@ -39,5 +39,6 @@ namespace RelationMovie.Models
         public DbSet<ProductLinkText> ProductLinkTexts { get; set; }
         public DbSet<Subscription> Subscriptions { get; set; }
         public DbSet<ProductItem> ProductItems { get; set; }
+        public DbSet<Test> Tests { get; set; }
     }
 }
